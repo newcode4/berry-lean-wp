b0f5c93be5cf67767a93e1ca6281dffd1c1c03b6
Sloa <past1303@gmail.com>
2025-08-27 11:41:19 +0900

    업데이트

--- berry-lean-wp/berry-lean.php
+++ berry-lean-wp/berry-lean.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Berry Lean Utilities
- * Version: 1.1.5
+ * Version: 1.1.6
  * Description: 초경량 공통 유틸 CSS(enqueue) + 사이트 변수/브랜드 
  * Author: Berrywalk
  * Requires PHP: 7.4
